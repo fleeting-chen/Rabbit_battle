0321369a9b1a74ef8fa225edef7f7c1893118403
fleeting_chen <chenhao0709@gmail.com>
2014-07-08 22:25:42 +0800

    增加了描述以及完成了terrain

--- classes/terrain.py
+++ classes/terrain.py
@@ -2,6 +2,11 @@ __author__ = 'chenhao'
 __date__ = '14-7-7'
 #encoding:utf-8
 
+MAP_WIDTH = 640
+MAP_HEIGHT = 480
+
 class Terrain(object):
 	def __init__(self):
-		self.width = 
+		self.width = MAP_WIDTH
+		self.height = MAP_HEIGHT
+		self.background = "../resources/images/grass.png"
