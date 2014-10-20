1044befd2f3f966ac2fc5e57e5a941397d0a3d5e
fleeting_chen <chenhao0709@gmail.com>
2014-10-20 22:05:21 +0800

    保存下历史记录

--- client.py
+++ client.py
@@ -2,27 +2,58 @@ __author__ = 'fleeting_chen'
 
 
 from socket import *
+import gevent
+from gevent.socket import *
 import math
 import pygame
 from pygame.locals import *
 from classes import terrain
+from classes.message import *
+from classes.base_server import BaseServer
+from proto import rabbit_pb2
 
 HOST = 'localhost'
 PORT = 9000
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
-client = socket(AF_INET, SOCK_STREAM)
-client.connect(ADDR)
-while True:
-	data = raw_input('>')
-	# data = test_pb2.Test()
-	# # # data.x = 1
-	# # data.y = 2
-	# test = data.SerializeToString()
-	client.sendall(data)
-	rec = client.recv(BUFSIZE)
-	if not rec:
-		break
-	print rec.strip()
-client.close()
+# client = socket(AF_INET, SOCK_STREAM)
+# client.connect(ADDR)
+# msg = 'start'
+# client.sendall(msg)
+# rabbits = []
+# while True:
+# 	rec = client.recv(BUFSIZE)
+# 	if not rec:
+# 		continue
+# 	msgs = rabbit_pb2.Rabbits()
+# 	msgs.ParseFromString(rec)
+# 	print msgs
+	# for rbt in msgs.rabbit:
+	# 	rabbits.append(protobuf_2_rabbit(rbt))
+	# print rabbits
+# client.close()
+
+class BattleClient(BaseServer):
+	def on_data(self, data):
+		print data
+		# msg = message.all_messages.pack_rabbit_message(rabbits)
+		# socket.send(msg)
+
+		# for rbt in rabbits:
+		# 	msg = rabbit_2_protobuf(rbt)
+		# 	socket.send(msg.SerializeToString())
+
+class ClientManage():
+	def _run(self):
+		client = socket(AF_INET, SOCK_STREAM)
+		client.connect(ADDR)
+		bc = BattleClient(client)
+		bc.start()
+
+		data = 'hello'
+		bc.put_data(data)
+		bc.send_data()
+
+cm = ClientManage()
+cm._run()
--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 __author__ = 'fleeting_chen'
 
-import SocketServer
-from SocketServer import StreamRequestHandler as SRH
+import gevent
+from gevent.server import StreamServer
+# import SocketServer
+# from SocketServer import StreamRequestHandler as SRH
 from classes.rabbit import Rabbit
 from classes.terrain import Terrain
+from classes import message
+from classes.base_server import BaseServer
+from proto import rabbit_pb2
 import random
+import struct
 
 rabbits = []
 for i in range(0,2):
@@ -13,21 +19,65 @@ for i in range(0,2):
 	tmp_rbt = Rabbit(1, [x, y])
 	rabbits.append(tmp_rbt)
 
-class BattleServer(SRH):
-	def handle(self):
-		print "connection from:", self.client_address
-		# test = test_pb2.Test()
-		# test.ParseFromString(data)
-		while True:
-			data = self.request.recv(1024)
-			if not data:
-				break
-			print data
-			print "RECV from ", self.client_address[0]
-			self.request.send(data.upper())
+# msgs = rabbit_pb2.Rabbits()
+# for rbt in rabbits:
+# 	msg = rabbit_2_protobuf(rbt)
+	# msgs.rabbit.append(msg)
+	# addRabbit(rbt, msgs.rabbit.add())
+# class BattleServer(SRH):
+# 	def handle(self):
+# 		print "connection from:", self.client_address
+# 		test = test_pb2.Test()
+		# while True:
+		# 	data = self.request.recv(1024)
+		# 	if not data:
+		# 		break
+		# 	print data
+		# 	print "RECV from ", self.client_address[0]
+		# 	self.request.send(data.upper())
 
-if __name__ == "__main__":
-	addr = ('localhost', 9000)
-	print 'server start:'
-	server = SocketServer.ThreadingTCPServer(addr, BattleServer)
-	server.serve_forever()
+# if __name__ == "__main__":
+# 	addr = ('localhost', 9000)
+# 	print 'server start:'
+# 	server = SocketServer.ThreadingTCPServer(addr, BattleServer)
+# 	server.serve_forever()
+
+class BattleServer(BaseServer):
+	# def handle(self, socket, address):
+	# 	print "connection from:", address
+	# 	while True:
+	# 		data = socket.recv(1024)
+	# 		if data == 'start':
+	# 			break
+	#
+	# 	msg = message.all_messages.pack_rabbit_message(rabbits)
+	# 	socket.send(msg)
+
+		# for rbt in rabbits:
+		# 	msg = rabbit_2_protobuf(rbt)
+		# 	socket.send(msg.SerializeToString())
+
+	def on_data(self, msg):
+		# data = rabbit_pb2.Rabbits()
+		# data.ParseFromString(msg)
+		print msg
+		self.put_data('hello,moto')
+		self.send_data()
+
+class ServerManage(gevent.Greenlet):
+	def __init__(self):
+		gevent.Greenlet.__init__(self)
+
+	def handle(self, my_socket, address):
+		print "connection from:", address
+		bs = BattleServer(my_socket)
+		bs.start()
+
+	def _run(self):
+		addr = ('localhost', 9000)
+		print 'server start:'
+		server = StreamServer(addr, self.handle)
+		server.serve_forever()
+
+SM = ServerManage()
+SM._run()
